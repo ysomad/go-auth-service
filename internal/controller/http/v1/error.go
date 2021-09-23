7ee2154833fa76b41bf6583d2f927a4de182eed9
Alex <alexeyheather@gmail.com>
2021-09-23 15:28:10 +0700

    validation translations refactoring

--- internal/controller/http/v1/error.go
+++ internal/controller/http/v1/error.go
@@ -2,6 +2,7 @@ package v1
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
 )
 
 type messageResponse struct {
@@ -18,4 +19,12 @@ func abortWithError(c *gin.Context, code int, err error) {
 
 func abortWithValidationError(c *gin.Context, code int, errs map[string]string) {
 	c.AbortWithStatusJSON(code, validationErrorResponse{errs})
+}
+
+type validationErrResponse struct {
+	Errors validator.ValidationErrorsTranslations `json:"error" example:"ModelName.FieldName:validation error message"`
+}
+
+func abortWithValidationErr(c *gin.Context, code int, errs validator.ValidationErrorsTranslations) {
+	c.AbortWithStatusJSON(code, validationErrResponse{errs})
 }
