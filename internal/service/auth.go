09e707e676247bad5ba3b9732c499ac0c7197369
Alex <alexeyheather@gmail.com>
2021-09-26 01:38:05 +0700

    fixed comments

--- internal/service/auth.go
+++ internal/service/auth.go
@@ -24,14 +24,14 @@ func (as *AuthService) ttlSeconds() int {
 	return int(as.sessionTTL.Seconds())
 }
 
-// unixExpiration returns unix time dependent on sessionRepo TTL from t
+// unixExpiration returns unix time dependent on sessionTTL from t
 func (as *AuthService) unixExpiration(t time.Time) int64 {
 	return t.Add(as.sessionTTL).Unix()
 }
 
-// verifyAccess compares current sessionRepo fingerprint, userRepo ip, userRepo agent with received values from client.
-// If any of fields are not the same token is considered invalid so userRepo should log in with email and password again
-// to receive new refresh token.
+// verifyAccess compares current session fingerprint, user ip, user agent with received values from client.
+// If any of fields are not the same, refresh token is considered invalid. User should log in with email and password
+// to receive new refresh token
 func (as *AuthService) verifyAccess(curr *entity.Session, s entity.SessionSecurityDTO) error {
 	if curr.ExpiresAt < time.Now().Unix() ||
 		curr.Fingerprint != s.Fingerprint ||
@@ -45,9 +45,9 @@ func (as *AuthService) verifyAccess(curr *entity.Session, s entity.SessionSecuri
 	return nil
 }
 
-// Login identifies userRepo by email, password and creates new refresh sessionRepo.
+// Login identifies user by email, password and creates new refresh session
 func (as *AuthService) Login(ctx context.Context, cred entity.UserCredentialsDTO, security entity.SessionSecurityDTO) (entity.JWT, error) {
-	// Get userRepo from db
+	// Get user from db
 	user, err := as.userRepo.GetByEmail(ctx, cred.Email)
 	if err != nil {
 		return entity.JWT{}, err
@@ -64,7 +64,7 @@ func (as *AuthService) Login(ctx context.Context, cred entity.UserCredentialsDTO
 		return entity.JWT{}, err
 	}
 
-	// Store sessionRepo in redis
+	// Create refresh session in redis
 	now := time.Now()
 	if err = as.sessionRepo.Create(ctx, &entity.Session{
 		RefreshToken: refreshToken,
@@ -93,20 +93,20 @@ func (as *AuthService) Login(ctx context.Context, cred entity.UserCredentialsDTO
 }
 
 // RefreshToken creates new access and refresh token pair with expiration time.
-// If one of the sessionRepo security fields is not the same as in the current update sessionRepo,
-// then the current sessionRepo is deleted and a new one is not created. User should log in with email and password again.
+// If one of session security fields is not the same as in the current refresh session,
+// then the current session is deleted and a new one is not created. User should log in with email and password again
 func (as *AuthService) RefreshToken(ctx context.Context, security entity.SessionSecurityDTO) (entity.JWT, error) {
 	currSession, err := as.sessionRepo.Get(ctx, security.RefreshToken)
 	if err != nil {
 		return entity.JWT{}, err
 	}
 
-	// Delete current sessionRepo from redis
+	// Delete current refresh session from redis
 	if err = as.sessionRepo.Terminate(ctx, currSession.RefreshToken); err != nil {
 		return entity.JWT{}, err
 	}
 
-	// Check userRepo agent, ip, fingerprint and refresh token lifetime, if it's expire return token expired error
+	// Check user agent, ip, fingerprint and refresh token lifetime
 	if err = as.verifyAccess(currSession, security); err != nil {
 		return entity.JWT{}, err
 	}
@@ -117,7 +117,7 @@ func (as *AuthService) RefreshToken(ctx context.Context, security entity.Session
 		return entity.JWT{}, err
 	}
 
-	// Create new sessionRepo in redis
+	// Create new refresh session in redis
 	now := time.Now()
 	if err = as.sessionRepo.Create(ctx, &entity.Session{
 		RefreshToken: refreshToken,
