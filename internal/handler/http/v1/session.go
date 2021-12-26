eda9b28d1f3c826c62d1faee577776a96a37826d
ysomad <malykhsey@gmail.com>
2021-12-26 16:35:44 +0700

    get all sessions handler

--- internal/handler/http/v1/session.go
+++ internal/handler/http/v1/session.go
@@ -1,20 +1,21 @@
 package v1
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 
 	"github.com/ysomad/go-auth-service/internal/service"
-	
-  "github.com/ysomad/go-auth-service/pkg/logger"
+
+	"github.com/ysomad/go-auth-service/pkg/logger"
 	"github.com/ysomad/go-auth-service/pkg/validation"
 )
 
 type sessionHandler struct {
 	log logger.Interface
 	validation.Validator
-	session service.Session
+	sessionService service.Session
 }
 
 func newSessionHandler(handler *gin.RouterGroup, l logger.Interface, v validation.Validator, s service.Session) {
@@ -32,8 +33,19 @@ func newSessionHandler(handler *gin.RouterGroup, l logger.Interface, v validatio
 }
 
 func (h *sessionHandler) get(c *gin.Context) {
-	panic("implement")
-	var sessions []string
+	aid, err := accountID(c)
+	if err != nil {
+		h.log.Error(fmt.Errorf("http - v1 - session - get - accountID: %w", err))
+		c.AbortWithStatus(http.StatusUnauthorized)
+		return
+	}
+
+	sessions, err := h.sessionService.GetAll(c.Request.Context(), aid)
+	if err != nil {
+		h.log.Error(fmt.Errorf("http - v1 - session - get: %w", err))
+		c.AbortWithStatus(http.StatusInternalServerError)
+		return
+	}
 
 	c.JSON(http.StatusOK, sessions)
 }
